11a4fa8854197be770491cfbc9e645787372c636
Николай Ануфриев <sch-nikolaj@yandex.ru>
2021-03-14 13:20:37 +0300

    Исправление ошибок Flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,13 @@
-import 	prompt
+"""
+Модуль Cli.py. Пока только функция welcom_user().
+
+welcom_user() - запрос имени пользователя и приветсвие его по имени.
+"""
+
+import prompt
+
 
 def welcom_user():
+    """Запрос имени пользователя и приветсвие его по имени."""
     name = prompt.string('May I have your name? ')
-    print('Hello, ' + name + '!')
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,19 @@
-#!/usr/bin/env python
+# это не шебанг!/usr/bin/env python
 
-import brain_games.cli
+"""
+Модуль brain_games.py Пока только приветсвие и запрос имени.
+
+main() - пока только эта функция.
+"""
+
+from brain_games.cli import welcom_user
+
 
 def main():
-    print("Welcome to the Brain Games!")
-    brain_games.cli.welcom_user()
+    """Главная функция."""
+    print('Welcome to the Brain Games!')
+    welcom_user()
+
 
 if __name__ == '__main__':
     main()
