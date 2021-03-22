ad0064208d510a3d183367ee82d57a0cf35f66db
Николай Ануфриев <sch-nikolaj@yandex.ru>
2021-03-22 14:41:45 +0300

    Изменена логика работы игры НОД

---
+++ brain_games/games/game_gcd.py
@@ -0,0 +1,29 @@
+"""Игра НОД."""
+
+from random import randint
+
+from prompt import string
+
+
+def game():
+    """
+    Игровой процесс.
+
+    Returns:
+        True или False, в зависимости от ответа пользователя.
+    """
+    number_a = randint(0, 100)
+    number_b = randint(0, 100)
+    print(f'Question: {number_a} {number_b}')
+    while number_a != 0 and number_b != 0:
+        if number_a > number_b:
+            number_a %= number_b
+        else:
+            number_b %= number_a
+    right_answer = number_a + number_b
+
+    answer = string('Your answer: ')
+    if answer == str(right_answer):
+        return True
+    print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
+    return False
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,73 +1,21 @@
-"""
-Игра наибольший общий делитель (НОД).
+"""Игра наибольший общий делитель (НОД)."""
 
-calc() - подсчет значени. Пользователю предлагается найти НОД двух чисел.
-"""
-
-from random import randint
-
-from prompt import string
-
-
-def calc(number_a, number_b):
-    """
-    Получение результата вычислений.
-
-    Args:
-        number_a: целое число.
-        number_b: целое число.
-
-    Returns:
-        наибольший ощий делитель.
-    """
-    print(f'Question: {number_a} {number_b}')
-    while number_a != 0 and number_b != 0:
-        if number_a > number_b:
-            number_a %= number_b
-        else:
-            number_b %= number_a
-    return number_a + number_b
-
-
-def compare(right_answer):
-    """
-    Получение ответа пользователя и сравнение его с текущим.
-
-    Args:
-        right_answer: правильный ответ
-
-    Returns:
-        True или False в зависимости от результата
-    """
-    answer = string('Your answer: ')
-    if answer == str(right_answer):
-        return True
-    answer = f'{answer} is wrong answer ;(. Correct answer was {right_answer}.'
-    print(answer)
-    return False
+from brain_games.cli import welcome_user
+from brain_games.games.game_engine import engine
+from brain_games.games.game_gcd import game
 
 
 def main():
     """Тело игры."""
     print('Welcome to the Brain Games!')
-    # запрос имени, приветсвие и правила игры
-    name = string('May I have your name? ')
+    name = welcome_user()
     print(f'Hello, {name}!')
-    print('What is the result of the expression?')
+    print('Find the greatest common divisor of given numbers.')
 
-    current_round = 1
-
-    while current_round <= 3:
-        right_answer = calc(randint(0, 100), randint(0, 100))
-
-        if compare(right_answer):
-            print('Correct!')
-            current_round += 1
-        else:
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f'Congratulations, {name}!')
+    if engine(game):
+        print(f'Congratulations, {name}!')
+    else:
+        print(f"Let's try again, {name}!")
 
 
 if __name__ == '__main__':
