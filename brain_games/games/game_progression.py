f1be8f677a8d564fe855f2d2e48262d532ba166b
Николай Ануфриев <sch-nikolaj@yandex.ru>
2021-03-23 21:13:41 +0300

    Изменил логику работы игры прогрессия

---
+++ brain_games/games/game_progression.py
@@ -0,0 +1,47 @@
+"""Игровой процесс игры арифметическая прогрессия."""
+
+from random import randint
+
+from prompt import string
+
+
+def game():
+    """
+    Игровой процесс.
+
+    Придумывается арифметическая прогрессия.
+    Выводится на экран без одного члена.
+    Нужно узнать его.
+
+    Returns:
+        True или False, в зависимости от ответа пользователя.
+    """
+    number_of_elements = 5 + randint(0, 5)
+    diff = randint(1, 10)
+    secret_item = randint(0, number_of_elements - 1)
+    right_answer = 0
+    count = 0
+    current_element = randint(0, 10)
+    arithmetic = ''
+
+    # Генерирование прогрессиии
+    while count < number_of_elements:
+        if count == secret_item:
+            arithmetic = f'{arithmetic}..'
+            right_answer = current_element
+        else:
+            arithmetic = f'{arithmetic}{current_element}'
+        # это пробелы между числами,
+        # если последний элемент, то пробел не будет ставится
+        if count != number_of_elements - 1:
+            arithmetic = f'{arithmetic} '
+        count += 1
+        current_element += diff
+
+    print(arithmetic)
+    answer = string('Your answer: ')
+    if answer == str(right_answer):
+        print('Correct!')
+        return True
+    print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
+    return False
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,95 +1,21 @@
-"""
-Игра арифметическая прогрессия.
+"""Скрипт запуска игры арифметическая прогрессия."""
 
-calc() - подсчет значений.
-Пользователю предлагается найти скрытый член арифметической прогрессии.
-"""
-
-from random import randint
-
-from prompt import string
-
-
-def calc():
-    """
-    Получение результата вычислений.
-
-    Returns:
-        Возвращает скрытый член арифметической прогрессии.
-    """
-    # прогрессия будет храниться в виде списка
-    arithmetic = []
-    count = 1
-    end = randint(4, 9)
-    # первый член прогрессии находим случайным образом
-    arithmetic.append(randint(0, 10))
-    # разность прогрессии
-    diff = randint(1, 10)
-    # заполнение
-    while count <= end:
-        arithmetic.append(arithmetic[count - 1] + diff)
-        count += 1
-
-    count = 0
-    arithmetic_str = ''
-    # индекс последнего элемента
-    end = len(arithmetic) - 1
-    secret_i = randint(0, end)
-    # логика такая, в цикле добавляю каждый элемент к строке,
-    # а секретный заменяю точками
-    while count <= end:
-        if count == secret_i:
-            arithmetic_str = f'{arithmetic_str}..'
-        else:
-            arithmetic_str = f'{arithmetic_str}{arithmetic[count]}'
-        # это пробелы между числами,
-        # если последний элемент, то пробел не будет ставится
-        if count != end:
-            arithmetic_str = f'{arithmetic_str} '
-        count += 1
-
-    print(f'Question: {arithmetic_str}')
-
-    return arithmetic[secret_i]
-
-
-def compare(right_answer):
-    """
-    Получение ответа пользователя и сравнение его с текущим.
-
-    Args:
-        right_answer: правильный ответ
-
-    Returns:
-        True или False в зависимости от результата
-    """
-    answer = string('Your answer: ')
-    if answer == str(right_answer):
-        return True
-    answer = f'{answer} is wrong answer ;(. Correct answer was {right_answer}.'
-    print(answer)
-    return False
+from brain_games.cli import welcome_user
+from brain_games.games.game_engine import engine
+from brain_games.games.game_progression import game
 
 
 def main():
     """Тело игры."""
     print('Welcome to the Brain Games!')
-    # запрос имени, приветсвие и правила игры
-    name = string('May I have your name? ')
+    name = welcome_user()
     print(f'Hello, {name}!')
-    print('What is the result of the expression?')
+    print('What number is missing in the progression?')
 
-    current_round = 1
-
-    while current_round <= 3:
-        if compare(calc()):
-            print('Correct!')
-            current_round += 1
-        else:
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f'Congratulations, {name}!')
+    if engine(game):
+        print(f'Congratulations, {name}!')
+    else:
+        print(f"Let's try again, {name}!")
 
 
 if __name__ == '__main__':
