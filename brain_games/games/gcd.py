a4eb7fb57d4b3ae778e66f43dc62d0857aa740d3
Николай Ануфриев <sch-nikolaj@yandex.ru>
2021-03-28 14:19:04 +0300

    Мелкие доработки

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,16 +8,16 @@ def game():
     Игровой процесс.
 
     Returns:
-        True или False, в зависимости от ответа пользователя.
+        Возвращает условие игры, вопрос и верный результат.
     """
-    print('Find the greatest common divisor of given numbers.')
+    condition = 'Find the greatest common divisor of given numbers.'
     number_a = randint(1, 100)
     number_b = randint(1, 100)
-    print(f'Question: {number_a} {number_b}')
+    question = f'Question: {number_a} {number_b}'
     while number_a != 0 and number_b != 0:
         if number_a > number_b:
             number_a %= number_b
         else:
             number_b %= number_a
 
-    return str(number_a + number_b)
+    return condition, question, str(number_a + number_b)
