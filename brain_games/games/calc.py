863bde315d2fa155690ae77dba4767e220525bea
Николай Ануфриев <sch-nikolaj@yandex.ru>
2021-03-28 14:18:58 +0300

    Мелкие доработки

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -11,14 +11,14 @@ def game():
     Пользователю нужно решить получившийся пример.
 
     Returns:
-        ВОзвращает результат.
+        Возвращает условие игры, вопрос и верный результат.
     """
-    print('What is the result of the expression?')
+    condition = 'What is the result of the expression?'
 
     number_a = randint(0, 100)
     number_b = randint(0, 100)
     operator = choice('+-*')
-    print(f'Question: {number_a} {operator} {number_b}')
+    question = f'Question: {number_a} {operator} {number_b}'
     if operator == '+':
         expected = number_a + number_b
     elif operator == '-':
@@ -26,4 +26,4 @@ def game():
     else:
         expected = number_a * number_b
 
-    return str(expected)
+    return condition, question, str(expected)
