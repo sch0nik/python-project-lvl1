437022edbdbf3dc1bdd64439b54cdb63bf55a8a7
Николай Ануфриев <sch-nikolaj@yandex.ru>
2021-04-03 11:18:38 +0300

    Мелкие правки

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -8,11 +8,7 @@ import prompt
 
 
 def welcome_user():
-    """Собственно сама функция.
-
-    Returns:
-        Возвращает имя пользователя.
-    """
+    """Собственно сама функция."""
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
